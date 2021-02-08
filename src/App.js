4c9b96e22fd0608cc7e51899217052a12b72d036
Shenica Graham <gshenica@gmail.com>
2021-02-07 23:44:01 -0600

    Out of time. Last commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+
+// Import dependencies
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './App.css';
-
 import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  const [data, setData] = useState([]);
+  const [characterData, setCharacterData] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -17,24 +18,25 @@ const App = () => {
   useEffect(() => {
 
       axios
-        .get('http://swapi.py4e.com/api/people/')
+        .get('https://swapi.dev/api/people/')
         .then(response => {
           console.log(response);
-          setData(response.data.results)
+          setCharacterData(response.data.results)
         })
 
         .catch((err => console.log(err))
         )
 
-  },[]); // Set an empty array as the second parameter to avoid an infite loop since we only want to fetch data once from the API.
+  },[]); // Set an empty array as the second parameter to avoid an infite loop.
+         // We only want to fetch data once from the API.
 
-    console.log(data);
+    console.log(characterData);
 
     return (
         <div className="App">
             <h1 className="Header">Characters</h1>
-            {data.map((char, index)=>
-                <Character key={char.url} data={data[index]}/>
+            {characterData.map((characterData, index)=>
+                <Character key={characterData.url} data={characterData[index]}/>
             )}
         </div>
     );
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,2 +1,72 @@
 // Write your Character component here
 
+// Import dependencies
+import React from "react";
+import styled from "styled-components";
+
+import {
+	Card,
+	CardBody,
+	CardHeader,
+	CardImageHeader,
+	CardText,
+} from "styled-card-component";
+
+const StyledCard = styled(Card)`
+   width 30%%;
+   margin: 0 auto;
+   height: 600px
+`;
+
+const StyledCardText = styled(CardText)`
+	padding: 2.5rem 0;
+	line-height: 1.2rem;
+`;
+
+const Character = (props) => {
+	const { character } = props;
+
+	const setStatus = () => {
+		switch (character.status) {
+			case "Alive":
+				return <span className="alive">Alive</span>;
+			case "Dead":
+				return <span className="dead">Dead</span>;
+			case "unknown":
+				return <span className="unknown">Unknown</span>;
+
+			default:
+				return <h1>error</h1>;
+		}
+	};
+	return (
+		<StyledCard>
+			<CardImageHeader src={character.image} alt="Character Image" />
+			<CardBody>
+				<CardHeader>
+					<h3>{character.name}</h3>
+				</CardHeader>
+				<StyledCardText>
+					<span className="bold">Species:</span> {character.species}
+					<br />
+					<span className="bold">Gender:</span> {character.gender}
+					<br />
+					<span className="bold">Origin:</span> {character.origin.name}
+					<br />
+                    <span className="bold">Height:</span> {character.height}
+					<br />
+                    <span className="bold">Eye Color:</span> {character.eye_color}
+					<br />
+                    <span className="bold">Hair Color:</span> {character.hair_color}
+					<br />
+					<span className="bold">Location:</span> {character.location.name}
+					<br />
+					<span className="bold">Status:</span> {setStatus()}
+					<br />
+				</StyledCardText>
+			</CardBody>
+		</StyledCard>
+	);
+};
+
+export default Character;
