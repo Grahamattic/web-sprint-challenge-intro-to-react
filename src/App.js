e71d1db56782c7105b7215af1867a19b223f74b6
Shenica Graham <gshenica@gmail.com>
2021-02-07 22:50:57 -0600

    Edited App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+import Character from './components/Character'
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  const [ data, setData ] = useState([]);
+  const [data, setData] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -14,22 +16,28 @@ const App = () => {
 
   useEffect(() => {
 
-    axios
-      .get('http://swapi.py4e.com/api/people/')
-      .then(response => {
-        console.log(response);
-        setData(response.data.results);
-      })
-      .catch(err => console.log(err))
+      axios
+        .get('http://swapi.py4e.com/api/people/')
+        .then(response => {
+          console.log(response);
+          setData(response.data.results)
+        })
+
+        .catch((err => console.log(err))
+        )
+
   },[]); // Set an empty array as the second parameter to avoid an infite loop since we only want to fetch data once from the API.
 
-  console.log(data);
+    console.log(data);
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="Header">Characters</h1>
+            {data.map((char, index)=>
+                <Character key={char.url} data={data[index]}/>
+            )}
+        </div>
+    );
 }
 
 export default App;
